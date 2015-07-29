0828117ab33eed1ee9597e7b7cacc2034c739a65
Paul van der Walt <paul@denknerd.org>
2015-07-29 10:50:41 -0600

    Exercise 3: starting

---
+++ exercise3.rkt
@@ -0,0 +1,91 @@
+#lang racket
+
+(require redex "common.rkt" "extend-lookup.rkt")
+
+(define-extended-language Assignments Lambda
+  (e ::= .... n + (void) (set! x e))
+  (n ::= natural))
+
+; (let ((x_1 x_2) ...) e_1 e_2) binds the current value of x_2 to x_1,
+; evaluates e_1, throws away its value, and finally evaluates e_2 
+(define-metafunction Assignments
+  let : ((x e) ...) e e -> e
+  [(let ([x_lhs e_rhs] ...) e_1 e_2)
+   ((lambda (x_lhs ...)
+      ((lambda (x_dummy) e_2) e_1))
+    e_rhs ...)
+   (where (x_dummy) ,(variables-not-in (term (e_1 e_2)) '(dummy)))])
+
+(define e1
+  (term
+   (lambda (x)
+     (lambda (y)
+       (let ([tmp x])
+         (set! x (+ y 1))
+         tmp)))))
+ 
+(define p-1 (term ((,e1 1) 2)))
+ 
+(define e2
+  (term
+   ((lambda (x)
+      (let ([tmp x])
+        (set! x y)
+        tmp))
+    (let ([tmp-z z])
+      (set! z (+ z 1))
+      (let ([tmp-y y])
+        (set! y tmp-z)
+        tmp-y)))))
+ 
+(define p-2
+  (term ((lambda (y) ((lambda (z) ,e2) 1)) 2)))
+
+(define-extended-language Assignments-s Assignments
+  (E ::= hole (v ... E e ...) (set! x E))
+  (σ ::= ((x v) ...))
+  (v ::= n + (void) (lambda (x ...) e)))
+ 
+
+(define s->βs
+  (reduction-relation
+   Assignments-s
+   #:domain (e σ)
+   (--> [(in-hole E x) σ]
+        [(in-hole E (lookup σ x)) σ])
+   (--> [(in-hole E (set! x v)) σ]
+        [(in-hole E (void)) (extend σ (x) (v))])
+   (--> [(in-hole E (+ n_1 n_2)) σ]
+        [(in-hole E ,(+ (term n_1) (term n_2))) σ])
+   (--> [(in-hole E ((lambda (x ..._n) e) v ..._n)) σ]
+        [(in-hole E e) (extend σ (x_new ...) (v ...))]
+        (where (x_new ...) ,(variables-not-in (term σ) (term (x ...)))))))
+
+
+(define-extended-language ImperativeCalculus Assignments-s
+  (e ::= .... (letrec ((x v) ...) e)))
+
+(module+ test
+  (define example1 (term (letrec ((x (lambda (iets) (y iets)))
+                                  (y (lambda (z)    (+ z 41))))
+                           (x (y 4)))))
+
+  (define example2 (term (let ((x 4)
+                               (y 5))
+                           (void)
+                           (+ x y))))
+
+  (define example3 (term (letrec ((x 4)
+                                  (y 5))
+                           (+ x y))))
+
+  (test-equal (redex-match? ImperativeCalculus e example1) #t)
+  (test-equal (redex-match? ImperativeCalculus e example2) #t)
+  (test-equal (redex-match? ImperativeCalculus e example3) #t)
+  )
+
+
+
+
+(module+ test
+  (test-results))
